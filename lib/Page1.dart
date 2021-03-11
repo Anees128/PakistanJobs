fcdad0a192e2cbf371b622bb5b804a5296d4a032
aneeskhanofficial4 <aneeskhanofficial4@gmail.com>
2021-03-11 16:13:13 +0500

    1122

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -33,7 +33,92 @@ class _Page1State extends State<Page1> {
                   color: Colors.grey[500]
                   ),
               child: Text("My demo styling"),
-            )
+            ),
+            Container(
+              margin: EdgeInsets.all(7),
+              padding: EdgeInsets.all(50),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.pinkAccent, // set border color
+                      width: 3.0), // set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(20.0)), // set rounded corner radius
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //     blurRadius: 10,
+                  //     color: Colors.black,
+                  //     offset: Offset(1, 3),
+                  //   ),
+                  // ] // make rounded corner of border
+                  color: Colors.grey[500]
+                  ),
+              child: Text("My demo styling"),
+            ),
+            Container(
+              margin: EdgeInsets.all(7),
+              padding: EdgeInsets.all(50),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.pink[800], // set border color
+                      width: 3.0), // set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(20.0)), // set rounded corner radius
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //     blurRadius: 10,
+                  //     color: Colors.black,
+                  //     offset: Offset(1, 3),
+                  //   ),
+                  // ] // make rounded corner of border
+                  color: Colors.grey[500]
+                  ),
+              child: Text("My demo styling"),
+            ),
+            Container(
+              margin: EdgeInsets.all(7),
+              padding: EdgeInsets.all(50),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.green[500], // set border color
+                      width: 3.0), // set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(20.0)), // set rounded corner radius
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //     blurRadius: 10,
+                  //     color: Colors.black,
+                  //     offset: Offset(1, 3),
+                  //   ),
+                  // ] // make rounded corner of border
+                  color: Colors.grey[400]
+                  ),
+              child: Text("My demo styling"),
+            ),
+            Container(
+              margin: EdgeInsets.all(7),
+              padding: EdgeInsets.all(50),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.pink[800], // set border color
+                      width: 3.0), // set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(20.0)), // set rounded corner radius
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //     blurRadius: 10,
+                  //     color: Colors.black,
+                  //     offset: Offset(1, 3),
+                  //   ),
+                  // ] // make rounded corner of border
+                  color: Colors.grey[500]
+                  ),
+              child: Text("My demo styling"),
+            ),
+
           ],
         ),
       ),
