430181de50cd50a2431b9a9a6e4a1e4481b6b3dd
aneeskhanofficial4 <aneeskhanofficial4@gmail.com>
2021-03-16 13:53:11 +0500

    changed

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -5,61 +5,90 @@ class Page1 extends StatefulWidget {
   _Page1State createState() => _Page1State();
 }
 
+List a = [
+  "https://upload.wikimedia.org/wikipedia/commons/c/c9/KP_logo.png",
+  "https://e7.pngegg.com/pngimages/378/720/png-clipart-government-of-punjab-pakistan-school-education-department-india-pakistan-border-leaf-text.png",
+  "https://pbs.twimg.com/profile_images/1132229943969091584/2LTjiBFC_400x400.jpg",
+  "https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_960_720.jpg",
+];
+
 class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 230,
-                decoration: BoxDecoration(
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 220,
+              decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   image: DecorationImage(
-                    image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/State_emblem_of_Pakistan.svg/1200px-State_emblem_of_Pakistan.svg.png'),
-                    fit: BoxFit.fitHeight
-                  )
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
-                        Colors.black.withOpacity(.4),
-                        Colors.black.withOpacity(.2),
-                      ]
-                    )
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text("Pakistan Jobs", style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 30,),
-                      // Container(
-                      //   height: 50,
-                      //   margin: EdgeInsets.symmetric(horizontal: 40),
-                      //   decoration: BoxDecoration(
-                      //     borderRadius: BorderRadius.circular(10),
-                      //     color: Colors.white
-                      //   ),
-                      //   child: Center(child: Text("Shop Now", style: TextStyle(color: Colors.grey[900], fontWeight: FontWeight.bold),)),
-                      // ),
-                      SizedBox(height: 30,),
+                      image: NetworkImage(
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_the_Prime_Minister_of_Pakistan.svg/1200px-Flag_of_the_Prime_Minister_of_Pakistan.svg.png'),
+                      fit: BoxFit.fitHeight)),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomRight,
+                    colors: [
+                      Colors.black.withOpacity(.2),
+                      Colors.black.withOpacity(.2),
                     ],
                   ),
                 ),
-                
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      "Pakistan Jobs",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
-              
-            ],
-          ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (BuildContext ctx, int index) {
+                  return Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Card(
+                      shape: Border.all(
+                        width: 5,
+                      ),elevation: 10,
+                   
+                      color: Colors.white,
+                      child: Column(
+                        children: <Widget>[
+                          Image.network(a[index]),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.yellow,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: a.length,
+              ),
+            ),
+          ],
         ),
-    )
+      ),
     );
   }
 }
--- lib/Page2.dart
+++ lib/Page2.dart
@@ -4,96 +4,62 @@ class Page2 extends StatefulWidget {
   @override
   _Page2State createState() => _Page2State();
 }
- final List<String> _listItem = [
-    'assets/images/two.png',
-    'images/three.png',
-    'images/four.png',
-    'images/five.png',
-    'images/one.png',
-    'images/two.png',
-    'images/three.png',
-    'images/four.png',
-    'images/five.png',
-  ];
+
+final List<String> _listItem = [
+  'images/pak1.png',
+  'images/pak2.png',
+  'images/pak3.png',
+  'images/pak4.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak1.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak5.png',
+  'images/pak1.png',
+];
 
 class _Page2State extends State<Page2> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                    image: NetworkImage('https://getinfopk.com/wp-content/uploads/Government-of-Pakistan-press-information.png'),
-                    fit: BoxFit.fitHeight
-                  )
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
-                        Colors.black.withOpacity(.4),
-                        Colors.black.withOpacity(.2),
-                      ]
-                    )
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Text("Lifestyle Sale", style: TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold),),
-                      SizedBox(height: 30,),
-                      SizedBox(height: 30,),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  children: _listItem.map((item) => Card(
-                    color: Colors.transparent,
-                    elevation: 1,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        image: DecorationImage(
-                          image: AssetImage(item),
-                          fit: BoxFit.cover
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(2),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 30,
+                    children: _listItem
+                        .map(
+                          (item) => Card(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(7),
+                                image: DecorationImage(
+                                    image: AssetImage(item),
+                                    fit: BoxFit.contain),
+                              ),
+                            ),
+                          ),
                         )
-                      ),
-                      child: Transform.translate(
-                        offset: Offset(50, -50),
-                        // child: Container(
-                        //   margin: EdgeInsets.symmetric(horizontal: 65, vertical: 63),
-                        //   decoration: BoxDecoration(
-                        //     borderRadius: BorderRadius.circular(10),
-                        //     color: Colors.white
-                        //   ),
-                        //   child: Icon(Icons.bookmark_border, size: 15,),
-                        // ),
-                      ),
-                    ),
-                  )).toList(),
-                )
-              )
-            ],
+                        .toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    )
     );
   }
 }
